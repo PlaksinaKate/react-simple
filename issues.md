31411d4db68f3b2d6a870d14e3fb12650c47c945
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-03-14 22:16:55 +0700

    feat: add button

--- issues.md
+++
@@ -1,6 +0,0 @@
-# План разработки:
-
-- [x] Изучить Feature-Sliced Design;
-- [x] Продумать архитектуру по Feature-Sliced Design;
-- [ ] Изучить  Firebase;
-
---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+interface IButton {
+  children: ReactNode;
+  onClick?: () => void;
+  [x: string]: unknown;
+}
+
+export function Button({ children, onClick, ...rest }: IButton) {
+  const buttonStyles =
+    "bg-green border-2 border-green min-w-min rounded-xl cursor-pointer py-3 px-5 text-white text-base font-semibold	transition-all duration-300 ease-out";
+  const buttonStylesHover = "hover:bg-dark-green hover:border-dark-green";
+  const buttonStylesFocus = "focus:border-2	focus:border-black";
+  return (
+    <button
+      className={`${buttonStyles} ${buttonStylesHover} ${buttonStylesFocus}`}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
