70f181e80aceb42096e21c2daa888e494e7fb77f
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-01 20:17:44 +0700

    feat: add wr to guest layout

--- src/pages/layouts/guest-layout.tsx
+++ src/pages/layouts/guest-layout.tsx
@@ -1,5 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 export function GuestLayout() {
-  return <Outlet />;
+  const wr = "flex items-center justify-self-center h-screen	w-screen bg-gray";
+  const guestWr = "w-full	max-w-[460px] my-0 mx-auto";
+  return (
+    <div className={wr}>
+      <div className={guestWr}>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
--- src/pages/registration/registration.ui.tsx
+++ src/pages/registration/registration.ui.tsx
@@ -2,22 +2,17 @@ import { Button, Input } from "../../shared/ui";
 
 export function Registration() {
   return (
-    <>
+    <div>
       <div>
         <h1>Регистрация</h1>
         <form>
           <Input label={"E-mail"} required value={""} type={""} />
-          <Input
-            label={"Придумайте пароль"}
-            required
-            value={""}
-            type={""}
-          />
+          <Input label={"Придумайте пароль"} required value={""} type={""} />
           <Input label={"Повторите пароль"} required value={""} type={""} />
           <Button>Зарегистрироваться</Button>
         </form>
       </div>
       <div>Уже зарегистрированы? Войти</div>
-    </>
+    </div>
   );
 }
