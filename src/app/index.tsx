fc3a6b2d10a1da5a2bbf90499b15dc33a854fbf4
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-12 00:27:50 +0700

    feat: add firebase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "./providers";
+import './firebase/index.js'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider />
