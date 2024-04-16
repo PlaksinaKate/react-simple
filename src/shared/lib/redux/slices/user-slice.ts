602e2444dd220f0f0ac68fc9086461d39cca15df
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-16 21:11:21 +0700

    feat: add set user and remove user

--- src/shared/lib/redux/slices/user-slice.ts
+++ src/shared/lib/redux/slices/user-slice.ts
@@ -6,9 +6,23 @@ const initialState = {
   id: null,
 };
 
-const useSlice = createSlice({
-  name: 'user',
+const userSlice = createSlice({
+  name: "user",
   initialState,
   reducers: {
+    setUser(state, action) {
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+      state.id = action.payload.id;
+    },
+    removeUser(state) {
+      state.email = null;
+      state.token = null;
+      state.id = null;
+    },
   },
-})
+});
+
+export const { setUser, removeUser } = userSlice.actions;
+
+export default userSlice.reducer;
