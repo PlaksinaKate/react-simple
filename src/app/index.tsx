e94c0a9fa1decb13463565e0e94b724c12b2b317
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-04-16 21:26:05 +0700

    feat: add redux provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "./providers";
+import { LocalProvider } from "./providers";
 import './firebase/index.js'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider />
+  <LocalProvider />
 );
--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from "./router-provider";
-import '../../shared/main.css'
+import "../../shared/main.css";
 import { withSuspense } from "../../shared/lib/react/hocs";
 import { LoadingIcon } from "../../shared/ui/Icons";
+import { Provider } from "react-redux";
+import { store } from "../../shared/lib/redux";
 
 function Providers() {
-  return <BrowserRouter />;
+  return (
+    <Provider store={store}>
+      <BrowserRouter />
+    </Provider>
+  );
 }
 
-const SuspensedProvider = withSuspense(Providers, {
+export const SuspensedProvider = withSuspense(Providers, {
   fallback: <LoadingIcon />,
 });
-export const Provider = SuspensedProvider;
+export const LocalProvider = SuspensedProvider;
